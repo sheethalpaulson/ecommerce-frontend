39f6e51d4a0ab394f0f058eabcf5bfdb7a6d941b
sheethalpaulson <sheethupaul@gmail.com>
2023-03-15 18:32:55 +0530

    cors error fixing

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,12 +1,16 @@
 import Axios from 'axios';
 
 let BASE_URL = "https://sheethal-ecommerce-services.onrender.com";
+BASE_URL = "http://localhost:4000";
 
 const axios = Axios.create({
   baseURL: BASE_URL,
   responseType: 'json',
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
+    'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, OPTIONS'
   },
   withCredentials: true,
 });
