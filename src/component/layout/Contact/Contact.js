8c01e6a52d5ec6dc9bae1fbafe7cfafbb0b3427a
sheethalpaulson <sheethupaul@gmail.com>
2023-03-16 10:16:17 +0530

    cors error fixing

--- src/component/layout/Contact/Contact.js
+++ src/component/layout/Contact/Contact.js
@@ -5,8 +5,8 @@ import { Button } from "@material-ui/core";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <a className="mailBtn" href="mailto:sheethalpaul803@gmail.com">
-        <Button>Contact: sheethalpaul803@gmail.com</Button>
+      <a className="mailBtn" href="mailto:sheethupaul@gmail.com">
+        <Button>Contact: sheethupaul@gmail.com</Button>
       </a>
     </div>
   );
