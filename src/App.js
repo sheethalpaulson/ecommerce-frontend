61bf4fb10aed3f7112f75b1e2b1fd77b821dbad8
sheethalpaulson <sheethupaul@gmail.com>
2023-03-29 09:40:28 +0530

    payment page not found

--- src/App.js
+++ src/App.js
@@ -67,6 +67,10 @@ function App() {
     getStripeApiKey();
   }, []);
 
+  if ( isAuthenticated && !stripeApiKey) {
+    getStripeApiKey();
+  }
+
   window.addEventListener("contextmenu", (e) => e.preventDefault());
 
   return (
